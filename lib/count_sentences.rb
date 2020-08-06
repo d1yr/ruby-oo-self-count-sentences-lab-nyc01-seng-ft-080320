09c6bc48562c9ebb7c9737194e0fa8a8a27197bb
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-06 04:47:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+      array = self.split(/\.|\?|\!./) #<----note to self: read more into regular expressions
+      return array.length
   end
+
 end
+
+
+#just passing learn with this to make it go all green, already #pushed via git commands
